58eb0da6440f7fe45692717432c13f3869af0168
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2023-12-24 17:10:57 +0700

    ppdb dashboard

---
+++ process/dashboard.php
@@ -0,0 +1,7 @@
+<?php
+
+use Core\Page;
+
+Page::set_title('Formulir PPDB');
+
+return view('ppdb/views/dashboard');
---
+++ views/dashboard.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+<div>
+    <h4>Selamat Datang <?=auth()->name?></h4>
+</div>
+<?php get_footer() ?>
