cebe711abfb38a75a6f2cc564974b68a4718e4f6
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2024-01-14 18:33:01 +0700

    fix export

--- hooks/additional-button-after-create-formulirs.php
+++ hooks/additional-button-after-create-formulirs.php
@@ -1,3 +1,3 @@
 <?php
 
-return '<a href="'.routeTo('formulirs/export').'" class="btn btn-sm btn-secondary" target="_blank">Export</a> ';
+return '<a href="'.routeTo('ppdb/formulirs/export').'" class="btn btn-sm btn-secondary" target="_blank">Export</a> ';
--- hooks/additional-button-after-create-registrations.php
+++ hooks/additional-button-after-create-registrations.php
@@ -1,3 +1,3 @@
 <?php
 
-return '<a href="'.routeTo('registrations/export').'" class="btn btn-sm btn-secondary" target="_blank">Export</a> ';
+return '<a href="'.routeTo('ppdb/registrations/export').'" class="btn btn-sm btn-secondary" target="_blank">Export</a> ';
